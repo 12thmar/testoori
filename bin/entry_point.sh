f740a1cd2d5d42eca32e69ff591df3c3de4f9c3d
seidom <sadem@numerahealth.com>
2014-12-15 09:23:43 -0800

    no wait

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -6,18 +6,7 @@ if [ ! -e /opt/selenium/config.json ]; then
   exit 1
 fi
 
-#function shutdown {
-#  kill -s SIGTERM $NODE_PID
-#  wait $NODE_PID
-#}
-
-# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
-
 xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR" \
   java -jar /opt/selenium/selenium-server-standalone.jar \
-       -log /var/log/selenium/selenium.log
        -nodeConfig /opt/selenium/config.json &
-NODE_PID=$!
 
-#trap shutdown SIGTERM SIGINT
-#wait $NODE_PID
